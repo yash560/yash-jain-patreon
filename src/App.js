4116f44fedb54850796a73ce92f0cc6fb8df32d3
yash560 <yaashjainn@gmail.com>
2021-06-19 07:55:08 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,270 @@
+import "./App.css";
+import "./firstScreen.css";
+import "./secondScreen.css";
+import "./thirdScreen.css";
+import "./fourthScreen.css";
+import "./fifthScreen.css";
+
+import Header from "./Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div class="Screen ">
+        <div class="firstScreen">
+          <div class="firstScreenComponent">
+            <img
+              height="100%"
+              width="100%"
+              src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/podcaster-v3-heathermcdonald.jpg"
+              alt=""
+            />
+          </div>
+          <div class="firstScreenComponent">
+            <div class="firstScreenComponentHeader">PATREON FOR PODCASTERS</div>
+            <div class="firstScreenComponentBody">
+              Get to know your most loyal listeners and give them a place to
+              connect, build community, and engage more deeply with your show.
+              Deliver exclusive episodes or bonus content, and establish a
+              reliable revenue stream beyond ads.
+            </div>
+            <div class="firstScreenComponentBody">
+              <div class="getStarted">Get Started</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="secondScreen">
+        <div class="secondScreenInner">
+          <div class="secondScreenHeader">
+            Why should podcasters use membership?
+          </div>
+          <div class="secondScreenBody">
+            <div class="secondScreenBodyComponent">
+              <div class="secondScreenImage">
+                <div class="secondScreenBodyIcon"></div>
+              </div>
+              <div class="secondScreenComponentBody">
+                <div class="secondScreenBodyComponentHeader">
+                  Make more money
+                </div>
+                <div class="secondScreenBodyComponentBody">
+                  Add another source of income by giving new and exclusive
+                  content to your biggest fans.
+                </div>
+              </div>
+            </div>
+            <div class="secondScreenBodyComponent">
+              <div class="secondScreenImage">
+                <div class="secondScreenBodyIcon"></div>
+              </div>{" "}
+              <div class="secondScreenComponentBody">
+                <div class="secondScreenBodyComponentHeader">
+                  Know your community
+                </div>
+                <div class="secondScreenBodyComponentBody">
+                  You own the relationship with your fans and get access to them
+                  directly. We don't stand in the way.
+                </div>{" "}
+              </div>
+            </div>
+            <div class="secondScreenBodyComponent">
+              <div class="secondScreenImage">
+                <div class="secondScreenBodyIcon"></div>
+              </div>{" "}
+              <div class="secondScreenComponentBody">
+                <div class="secondScreenBodyComponentHeader">
+                  Be independent
+                </div>
+                <div class="secondScreenBodyComponentBody">
+                  Live in a world not reliant on ads or paid sponsorships to
+                  keep your business running.
+                </div>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="thirdScreen">
+        <div class="thirdScreenInner">
+          <div class="thirdScreenHeader">Popular features for podcasters </div>
+          <div class="thirdScreenBody">
+            <div class="thirdScreenBodyComponent">
+              <div class="thirdScreenBodyIcon">
+                <img
+                  class="thirdScreenImage"
+                  height="300px"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-01_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="thirdScreenBodyComponentHeader">Private RSS feed</div>
+              <div class="thirdScreenBodyComponentBody">
+                Securely share your early access and bonus episodes with your
+                fans.
+              </div>
+            </div>
+            <div class="thirdScreenBodyComponent">
+              <div class="thirdScreenBodyIcon">
+                <img
+                  height="300px"
+                  class="thirdScreenImage"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-02_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="thirdScreenBodyComponentHeader">
+                Management of benefits
+              </div>
+              <div class="thirdScreenBodyComponentBody">
+                Easily track and fulfill membership benefits quickly.
+              </div>
+            </div>
+            <div class="thirdScreenBodyComponent">
+              <div class="thirdScreenBodyIcon">
+                <img
+                  class="thirdScreenImage"
+                  height="300px"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-03_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="thirdScreenBodyComponentHeader">
+                Community & messaging
+              </div>
+              <div class="thirdScreenBodyComponentBody">
+                Tools to message and engage with your members.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fourthScreen">
+        <div class="fourthScreenInner">
+          <div class="fourthScreenHeader">Notable podcasters on Patreon </div>
+          <div class="fourthScreenBody">
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://c10.patreonusercontent.com/3/eyJ3IjoyMDB9/patreon-media/p/campaign/1766401/e034374751e44b7f93cbebd6b166ca7b/1.jpg?token-time=2145916800&token-hash=pYUFyaH3tsnslDhIytknMa7_PLN9UtwBzUrXqRk3C0g%3D"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">
+                Welcome to Night Vale
+              </div>
+            </div>
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://c10.patreonusercontent.com/3/eyJ3IjoyMDB9/patreon-media/p/campaign/2369690/d82e14884819467cb6d881961b4185f3/6.png?token-time=2145916800&token-hash=-8Z1k2SOuUmlsKvrLtjvmyy9nPvnH9FQ9mBC4dEYOlA%3D"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">
+                Dungeons and Daddles
+              </div>
+            </div>
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://c10.patreonusercontent.com/3/eyJ3IjoyMDB9/patreon-media/p/campaign/225959/a356c84c8db040a0a0d974d8c20fe8ee/1.jpeg?token-time=2145916800&token-hash=MGBN7rmh9nxdwLoqjeaI6ybGq2-Eew0mt3nbKyLbIdk%3D"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">DeadEndHipHop </div>
+            </div>
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://c10.patreonusercontent.com/3/eyJ3IjoyMDB9/patreon-media/p/campaign/2023945/07ab04bfc6b84f43b5e134d4882f2f51/1.jpg?token-time=2145916800&token-hash=ePXDfoB8ebQJDiK34qLsH22hwiugSjYKzlmOO_-cht0%3D"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">
+                2 Back Girls, 1 Rose
+              </div>
+            </div>{" "}
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-01_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">
+                Crime Junkie Podcast
+              </div>
+            </div>
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-02_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">Snap Judgement </div>
+            </div>
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-03_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">
+                Mueller, She Wrote & The <br />
+                Daily Beans
+              </div>
+            </div>
+            <div class="fourthScreenBodyComponent">
+              <div class="fourthScreenBodyIcon">
+                <img
+                  class="fourthScreenImage"
+                  src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/For-podcasters-03_2x.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="fourthScreenBodyComponentHeader">Amir and Jake </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fifthScreen">
+        <div class="fifthScreenInner">
+          <div class="fifthScreenBody">
+            <div class="fifthScreenBodyLeft">
+              <div class="fifthScreenPeople"></div>
+              <div class="fifthScreenPeopleName">
+                Heather
+                <br /> MCDonald
+              </div>
+              <div class="fifthScreenPeopleInfo">Juicy Scoop Podcast</div>
+            </div>
+            <div class="fifthScreenBodyRight">
+              "With Patreon, it's not about likes and views, you can really
+              connect with your fans, ignore the haters and focus on the
+              positive. I'd rather have 300 patrons than 3 million Instagram
+              followers."
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+      <div class="sixthScreen">
+        <div class="thirdScreenHeader">
+          Ready to build your membership business?
+        </div>{" "}
+        <div class="getStarted buttonScreensSixth">Get Started</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
